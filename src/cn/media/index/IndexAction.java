2f30e8a3251db5cad00e310a81a96d7a63312db6
ihanhao <ihanhao@foxmail,com>
2016-07-06 23:09:39 +0800

    前台页面优化

--- src/cn/media/index/IndexAction.java
+++ src/cn/media/index/IndexAction.java
@@ -1,5 +1,11 @@
 package cn.media.index;
 
+import java.util.List;
+
+import cn.media.category.CategoryService;
+import cn.media.multmedia.MultMedia;
+import cn.media.multmedia.MultMediaService;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -8,6 +14,33 @@ import com.opensymphony.xwork2.ActionSupport;
  *
  */
 public class IndexAction extends ActionSupport{
+
+	// 注入一级分类的Service
+	private CategoryService categoryService;
+	// 注入Media的Service
+	private MultMediaService multMediaService;
+	//热门
+	private List<MultMedia> hotList;
+	//最新
+	private List<MultMedia> newList;
+	
+	public List<MultMedia> getHotList() {
+		return hotList;
+	}
+
+	public List<MultMedia> getNewList() {
+		return newList;
+	}
+
+	public void setCategoryService(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+
+	public void setMultMediaService(MultMediaService multMediaService) {
+		this.multMediaService = multMediaService;
+	}
+
+
 
 	/**
 	 * 执行首页访问的方法
---
+++ src/cn/media/multmedia/MultMediaAction.java
@@ -0,0 +1,5 @@
+package cn.media.multmedia;
+
+public class MultMediaAction {
+
+}
