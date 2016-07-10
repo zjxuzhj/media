c4dc1b5774b2b0d912242f87cc0aec8379da5490
hukaijun <857929558@qq.com>
2016-07-10 23:50:59 +0800

    后台：查询一级分类

--- src/cn/media/category/CategoryAction.java
+++ src/cn/media/category/CategoryAction.java
@@ -1,5 +1,33 @@
 package cn.media.category;
 
-public class CategoryAction {
+import java.util.List;
 
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class CategoryAction extends ActionSupport implements ModelDriven<Category>{
+	// 模型驱动使用的类:
+		private Category category = new Category();
+
+		public Category getModel() {
+			return category;
+		}
+		// 注入Service
+		private CategoryService categoryService;
+		
+		public void setCategoryService(CategoryService categoryService) {
+			this.categoryService = categoryService;
+		}
+
+		/**
+		 * 后台:查询所有的一级分类的方法:
+		 */
+		public String adminFindAll(){
+			List<Category> cList = categoryService.findAll();
+			// 压入值栈:
+			ActionContext.getContext().getValueStack().set("cList", cList);
+			return "adminFindAllSuccess";
+		}
+		
 }
